7f16844a279bb8403eaa6269ea79b043ebee1bd0
Jackson Rodrigues <jjr0910@gmail.com>
2022-07-16 00:09:57 -0300

    First commit

---
+++ backend/src/main/java/com/mobi7/apimobi/ApiMobiApplication.java
@@ -0,0 +1,13 @@
+package com.mobi7.apimobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMobiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMobiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/mobi7/apimobi/ApiMobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobi7.apimobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMobiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
